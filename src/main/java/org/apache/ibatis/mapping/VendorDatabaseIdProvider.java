0b0c11c0b5ef15edea988513145d5bab1aabba09
刘景平 <liujingping@idwzx.com>
2019-10-18 15:47:48 +0800

    添加了对Mybatis初始化加载mybatis-config配置文件的分析与注释， 1.通过SqlSessionFactoryBuilder创建SqlSessionFactory时会对配置文件进行解析，进行一系列化的初始化操作，将相关数据添加到对应的对象中，其中Configuration为Mybatis的全局对象，其中几乎包含了所有的配置信息； 2.通过生成的Configuration对象生成对应的DefaultSqlSessionFactory； 3.与spring结合时SqlSessionFactoryBuilder由org.mybatis.spring.SqlSessionFactoryBean创建

--- src/main/java/org/apache/ibatis/mapping/VendorDatabaseIdProvider.java
+++ src/main/java/org/apache/ibatis/mapping/VendorDatabaseIdProvider.java
@@ -29,69 +29,74 @@ import org.apache.ibatis.logging.LogFactory;
 /**
  * Vendor DatabaseId provider.
  *
- * It returns database product name as a databaseId.
- * If the user provides a properties it uses it to translate database product name
- * key="Microsoft SQL Server", value="ms" will return "ms".
- * It can return null, if no database product name or
- * a properties was specified and no translation was found.
+ * It returns database product name as a databaseId. If the user provides a
+ * properties it uses it to translate database product name key="Microsoft SQL
+ * Server", value="ms" will return "ms". It can return null, if no database
+ * product name or a properties was specified and no translation was found.
  *
  * @author Eduardo Macarron
  */
 public class VendorDatabaseIdProvider implements DatabaseIdProvider {
 
-  private Properties properties;
+	private Properties properties;
 
-  @Override
-  public String getDatabaseId(DataSource dataSource) {
-    if (dataSource == null) {
-      throw new NullPointerException("dataSource cannot be null");
-    }
-    try {
-      return getDatabaseName(dataSource);
-    } catch (Exception e) {
-      LogHolder.log.error("Could not get a databaseId from dataSource", e);
-    }
-    return null;
-  }
+	@Override
+	public String getDatabaseId(DataSource dataSource) {
+		if (dataSource == null) {
+			throw new NullPointerException("dataSource cannot be null");
+		}
+		try {
+			// 获得数据库标识
+			return getDatabaseName(dataSource);
+		} catch (Exception e) {
+			LogHolder.log.error("Could not get a databaseId from dataSource", e);
+		}
+		return null;
+	}
 
-  @Override
-  public void setProperties(Properties p) {
-    this.properties = p;
-  }
+	@Override
+	public void setProperties(Properties p) {
+		this.properties = p;
+	}
 
-  private String getDatabaseName(DataSource dataSource) throws SQLException {
-    String productName = getDatabaseProductName(dataSource);
-    if (this.properties != null) {
-      for (Map.Entry<Object, Object> property : properties.entrySet()) {
-        if (productName.contains((String) property.getKey())) {
-          return (String) property.getValue();
-        }
-      }
-      // no match, return null
-      return null;
-    }
-    return productName;
-  }
+	private String getDatabaseName(DataSource dataSource) throws SQLException {
+		// <1> 获得数据库产品名
+		String productName = getDatabaseProductName(dataSource);
+		if (this.properties != null) {
+			for (Map.Entry<Object, Object> property : properties.entrySet()) {
+				// 如果产品名包含 KEY ，则返回对应的  VALUE
+				if (productName.contains((String) property.getKey())) {
+					return (String) property.getValue();
+				}
+			}
+			// no match, return null
+			return null;
+		}
+		// <3> 不存在 properties ，则直接返回 productName
+		return productName;
+	}
 
-  private String getDatabaseProductName(DataSource dataSource) throws SQLException {
-    Connection con = null;
-    try {
-      con = dataSource.getConnection();
-      DatabaseMetaData metaData = con.getMetaData();
-      return metaData.getDatabaseProductName();
-    } finally {
-      if (con != null) {
-        try {
-          con.close();
-        } catch (SQLException e) {
-          // ignored
-        }
-      }
-    }
-  }
+	private String getDatabaseProductName(DataSource dataSource) throws SQLException {
+		Connection con = null;
+		try {
+			// 获得数据库连接
+			con = dataSource.getConnection();
+			DatabaseMetaData metaData = con.getMetaData();
+			// 获得数据库产品名
+			return metaData.getDatabaseProductName();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// ignored
+				}
+			}
+		}
+	}
 
-  private static class LogHolder {
-    private static final Log log = LogFactory.getLog(VendorDatabaseIdProvider.class);
-  }
+	private static class LogHolder {
+		private static final Log log = LogFactory.getLog(VendorDatabaseIdProvider.class);
+	}
 
 }
--- src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
+++ src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -32,64 +32,75 @@ import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
  */
 public class SqlSessionFactoryBuilder {
 
-  public SqlSessionFactory build(Reader reader) {
-    return build(reader, null, null);
-  }
+	public SqlSessionFactory build(Reader reader) {
+		return build(reader, null, null);
+	}
 
-  public SqlSessionFactory build(Reader reader, String environment) {
-    return build(reader, environment, null);
-  }
+	public SqlSessionFactory build(Reader reader, String environment) {
+		return build(reader, environment, null);
+	}
 
-  public SqlSessionFactory build(Reader reader, Properties properties) {
-    return build(reader, null, properties);
-  }
+	public SqlSessionFactory build(Reader reader, Properties properties) {
+		return build(reader, null, properties);
+	}
 
-  public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
-    try {
-      XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
-      return build(parser.parse());
-    } catch (Exception e) {
-      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
-    } finally {
-      ErrorContext.instance().reset();
-      try {
-        reader.close();
-      } catch (IOException e) {
-        // Intentionally ignore. Prefer previous error.
-      }
-    }
-  }
+	/**
+	 * 构造 SqlSessionFactory 对象
+	 *
+	 * @param reader Reader 对象
+	 * @param environment 环境
+	 * @param properties Properties 变量
+	 * @return SqlSessionFactory 对象
+	 */
+	public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
+		try {
+			// <1> 创建 XMLConfigBuilder 对象
+			XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+			// <2> 执行 XML 解析
+	        // <3> 创建 DefaultSqlSessionFactory 对象
+			return build(parser.parse());
+		} catch (Exception e) {
+			throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+		} finally {
+			ErrorContext.instance().reset();
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// Intentionally ignore. Prefer previous error.
+			}
+		}
+	}
 
-  public SqlSessionFactory build(InputStream inputStream) {
-    return build(inputStream, null, null);
-  }
+	public SqlSessionFactory build(InputStream inputStream) {
+		return build(inputStream, null, null);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, String environment) {
-    return build(inputStream, environment, null);
-  }
+	public SqlSessionFactory build(InputStream inputStream, String environment) {
+		return build(inputStream, environment, null);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, Properties properties) {
-    return build(inputStream, null, properties);
-  }
+	public SqlSessionFactory build(InputStream inputStream, Properties properties) {
+		return build(inputStream, null, properties);
+	}
 
-  public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
-    try {
-      XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
-      return build(parser.parse());
-    } catch (Exception e) {
-      throw ExceptionFactory.wrapException("Error building SqlSession.", e);
-    } finally {
-      ErrorContext.instance().reset();
-      try {
-        inputStream.close();
-      } catch (IOException e) {
-        // Intentionally ignore. Prefer previous error.
-      }
-    }
-  }
+	public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
+		try {
+			XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
+			return build(parser.parse());
+		} catch (Exception e) {
+			throw ExceptionFactory.wrapException("Error building SqlSession.", e);
+		} finally {
+			ErrorContext.instance().reset();
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// Intentionally ignore. Prefer previous error.
+			}
+		}
+	}
 
-  public SqlSessionFactory build(Configuration config) {
-    return new DefaultSqlSessionFactory(config);
-  }
+	public SqlSessionFactory build(Configuration config) {
+		return new DefaultSqlSessionFactory(config);
+	}
 
 }
